8036ffe31d2b74d0d917d5e0ae5b5348d272979f
shawn <mingyuemyheart@126.com>
2020-06-23 08:39:52 +0800

    适配ConstraintLayout布局引起界面显示不全问题

--- app/build/generated/source/buildConfig/release/com/warning/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/warning/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.warning";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 83;
-  public static final String VERSION_NAME = "2.6.2";
+  public static final int VERSION_CODE = 84;
+  public static final String VERSION_NAME = "2.6.3";
 }
