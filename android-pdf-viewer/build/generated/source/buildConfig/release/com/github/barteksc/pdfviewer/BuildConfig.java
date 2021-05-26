4f89ffe1a25a2efd3d54a38c469180c4311d7756
shawn <mingyuemyheart@126.com>
2021-05-26 18:58:59 +0800

    增加预警类型

--- android-pdf-viewer/build/generated/source/buildConfig/release/com/github/barteksc/pdfviewer/BuildConfig.java
+++ android-pdf-viewer/build/generated/source/buildConfig/release/com/github/barteksc/pdfviewer/BuildConfig.java
@@ -5,6 +5,11 @@ package com.github.barteksc.pdfviewer;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.github.barteksc.pdfviewer";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "com.github.barteksc.pdfviewer";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
